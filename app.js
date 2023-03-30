223022bbbe45f3174622c308c8348a876fcbd335
talemanjan <talemanaptech@gmail.com>
2023-03-30 22:10:40 +0500

    added js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Defining a object which will hold some words and their definations
+const dictionary = {
+    hello: 'used as a greeting or to begin a conversation',
+    world: 'the earth, together with all of its countries, peoples, and natural features',
+    javascript: 'a high-level programming language used to create interactive effects within web browsers'
+  };
+  
+  
