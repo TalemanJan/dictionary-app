cd362cc518c83ae30d1bfdc8bbf54522020d572c
talemanjan <talemanaptech@gmail.com>
2023-03-31 18:25:06 +0500

    added js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const dictionary = {
     hello: 'used as a greeting or to begin a conversation',
     world: 'the earth, together with all of its countries, peoples, and natural features',
-    javascript: 'a high-level programming language used to create interactive effects within web browsers'
+    javascript: 'a high-level programming language used to create interactive effects within web browsers',
+word:"a command, password, or signal.",
+now:"at the present time or moment."
   };
   
   
@@ -15,4 +17,6 @@ const dictionary = {
     document.getElementById('p').innerHTML='Word Not found';
   }
   }
+  
+
   
